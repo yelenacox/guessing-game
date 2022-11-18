baa6f70562642276e25a60d95df24711b1c71f00
Yelena Cox <yelenacox@gmail.com>
2022-11-18 15:40:25 -0600

    first commmit

---
+++ guessing-game/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the secret number");
+            string UserGuess = Console.ReadLine();
+            int ParsedGuess = int.Parse(UserGuess);
+            Console.WriteLine(ParsedGuess);
+        }
+    }
+}

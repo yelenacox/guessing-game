bf67e4f6ae86efb10bc95b3991e559cb6255f98b
Yelena Cox <yelenacox@gmail.com>
2022-11-21 11:20:52 -0600

    finished phase 7

--- guessing-game/Program.cs
+++ guessing-game/Program.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+
+Console.Write("Difficulty? (1 - easy, 2 - medium, 3 - hard)");
+int difficulty = int.Parse(Console.ReadLine());
+List<int> difficulties = new List<int> { 8, 6, 4 };
+int numberOfGuesses = difficulties[difficulty - 1];
+
 
 Console.WriteLine("Guess the Secret Number!");
 
 int SecretNumber = new Random().Next(1, 101);
-Console.WriteLine(SecretNumber);
-for (int i = 4; i >= 1; i--)
+
+for (int i = numberOfGuesses; i >= 1; i--)
 {
     Console.Write($"Your Guess (Guesses left: {i}): ");
     int answer = int.Parse(Console.ReadLine());
